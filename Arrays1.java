5668f277dbfffe48e6d7b734b5cbe56d1b555389
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2025-05-10 20:29:50 +0530

    second commit

--- Arrays1.java
+++ Arrays1.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 class Arrays1{
     public static void main(String [] args){
-        System.out.println("Hello Arrays");
+        System.out.println("Hello Arrays This contains the array problems solved");
     }
 }
