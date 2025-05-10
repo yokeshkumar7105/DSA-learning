9ba7cdbe02dea583d5ee5578b797685969ed8d39
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2025-05-10 20:39:39 +0530

    reverse array method added

--- Arrays1.java
+++ Arrays1.java
@@ -1,7 +1,34 @@
 import java.util.*;
 
 class Arrays1{
+    public static void reverseArray(int [] arr){
+        System.out.println("Reversing the Array");
+        int i = 0;
+        int j = arr.length-1;
+
+        while(i<j){
+            arr[i] = arr[i]+arr[j];
+            arr[j] = arr[i]-arr[j];
+            arr[i] = arr[i]-arr[j];
+            i++;
+            j--;
+        }
+
+        for(int k=0;k<arr.length;k++){
+            System.out.print(arr[k]+" ");
+        }
+    }
+
+
+
     public static void main(String [] args){
-        System.out.println("Hello Arrays This contains the array problems solved");
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        reverseArray(arr);
     }
 }
