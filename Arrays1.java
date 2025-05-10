3ac3fd2a07c6ee3a0f5d8b20b48756486f7e29b1
yokeshkumar7105 <yokeshkumarcse@gmail.com>
2025-05-10 20:49:30 +0530

    minmaxElement in a array

--- Arrays1.java
+++ Arrays1.java
@@ -1,6 +1,15 @@
 import java.util.*;
 
 class Arrays1{
+
+    public static void printArray(int [] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    //REVERSING THE ARRAY
     public static void reverseArray(int [] arr){
         System.out.println("Reversing the Array");
         int i = 0;
@@ -14,9 +23,19 @@ class Arrays1{
             j--;
         }
 
-        for(int k=0;k<arr.length;k++){
-            System.out.print(arr[k]+" ");
+        printArray(arr);
+    }
+
+    //MAXIMUM AND MINIMUM ELEMENT IN ARRAY
+    public static void maxminElement(int [] arr){
+        System.out.println("Finding Min and Max element in array");
+        int min = arr[0], max = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min) min = arr[i];
+            else if(arr[i]>max) max = arr[i];
         }
+        System.out.println("Min : "+min);
+        System.out.println("Max : "+ max);
     }
 
 
@@ -30,5 +49,6 @@ class Arrays1{
             arr[i] = sc.nextInt();
         }
         reverseArray(arr);
+        maxminElement(arr);
     }
 }
